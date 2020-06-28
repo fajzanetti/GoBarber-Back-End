1de9497505d168aae1c9afbc56eaa1a2ffab8d1f
Felipe Zanetti <felipeajz@hotmail.com>
2020-06-28 02:48:23 -0300

    Upload de avatar no bando de dados

--- Server/src/routes/users.routes.ts
+++ Server/src/routes/users.routes.ts
@@ -2,7 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
@@ -33,7 +36,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.send();
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
