8214a86b9bcb928825df1cbb9038723fe0bf814d
Felipe Zanetti <felipeajz@hotmail.com>
2020-07-28 16:06:18 -0300

    TDD: configurando jest e primeiro teste

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,25 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  // isto deve poder criar um novo compromisso
+  it('should be able to create a new appointmet', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+  });
+
+  // // isso não deve ser capaz de criar dois compromissos ao mesmo tempo
+  // it('should not be able to create two appointmet on the same time', () => {
+
+  // });
+});
